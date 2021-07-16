bcd2b64a6bc283a1c8529b9be2f7faca7bd6898b
Victorio Iglesias Gutierrez <victorio.iglesias@the-cocktail.com>
2021-07-16 08:40:28 +0200

    Add tests and changes for remove elements in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,9 +12,13 @@ class CartsController < ApplicationController
   end
 
   def remove
-    flash[:notice] = "Product removed successfully"
     session[:cart] ||= []
-    session[:cart].delete_at(session[:cart].index(params[:product_id]) || session[:cart].length)
+    if session[:cart].include? params[:product_id]
+      session[:cart].delete_at(session[:cart].index(params[:product_id]) || session[:cart].length)
+      flash[:notice] = "Product removed successfully"
+    else
+      flash[:notice] = "Product not found"
+    end
     redirect_to products_path
   end
 
--- spec/features/cart/remove_product_spec.rb
+++ spec/features/cart/remove_product_spec.rb
@@ -5,13 +5,21 @@ RSpec.describe "When a user remove a product from the cart" do
         visit "/products"
 
         within("#product-#{product.id}") do
-        click_button "Remove Product"
+          click_button "Add Product"
+          click_button "Remove Product"
         end
 
         expect(page).to have_content("Product removed successfully")
+        
+        within("#product-#{product.id}") do
+          click_button "Remove Product"
+        end
+        
+        expect(page).not_to have_content("Product removed successfully")
+        expect(page).to have_content("Product not found")
     end
 
-    it "Decrement cart items counter" do
+    it "Decrement cart items counter when element exists" do
         product = Product.create(code: 'GR1', name: "Green Tea", price: 3.11)
         product2 = Product.create(code: 'SR1', name: "Strawberries", price: 5.00)
         product3 = Product.create(code: 'CF1', name: "Coffe", price: 11.23)
@@ -43,5 +51,41 @@ RSpec.describe "When a user remove a product from the cart" do
         end
 
         expect(page).to have_content("Items in cart: 1")
+        
+    end
+
+    it "Decrement cart items counter when element dont exists" do
+        product = Product.create(code: 'GR1', name: "Green Tea", price: 3.11)
+        product2 = Product.create(code: 'SR1', name: "Strawberries", price: 5.00)
+        product3 = Product.create(code: 'CF1', name: "Coffe", price: 11.23)
+    
+        visit "/products"
+    
+        expect(page).to have_content("Items in cart: 0")
+    
+        within("#product-1") do
+          click_button "Add Product"
+        end
+    
+        within("#product-2") do
+          click_button "Add Product"
+        end
+    
+        within("#product-1") do
+          click_button "Add Product"
+        end
+        
+        expect(page).to have_content("Items in cart: 3")
+
+        within("#product-3") do
+          click_button "Remove Product"
+        end
+        expect(page).to have_content("Items in cart: 3")
+        within("#product-3") do
+          click_button "Remove Product"
+        end
+        expect(page).to have_content("Items in cart: 3")
+        
+        
     end
 end
