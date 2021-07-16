773430fd721e5cb58b1e6c6cbf094c2759f93592
Victorio Iglesias Gutierrez <victorio.iglesias@the-cocktail.com>
2021-07-16 08:25:01 +0200

    Decrement items counter test

--- spec/features/cart/remove_product_spec.rb
+++ spec/features/cart/remove_product_spec.rb
@@ -10,4 +10,38 @@ RSpec.describe "When a user remove a product from the cart" do
 
         expect(page).to have_content("Product removed successfully")
     end
+
+    it "Decrement cart items counter" do
+        product = Product.create(code: 'GR1', name: "Green Tea", price: 3.11)
+        product2 = Product.create(code: 'SR1', name: "Strawberries", price: 5.00)
+        product3 = Product.create(code: 'CF1', name: "Coffe", price: 11.23)
+    
+        visit "/products"
+    
+        expect(page).to have_content("Items in cart: 0")
+    
+        within("#product-1") do
+          click_button "Add Product"
+        end
+    
+        within("#product-2") do
+          click_button "Add Product"
+        end
+    
+        within("#product-1") do
+          click_button "Add Product"
+        end
+        
+        expect(page).to have_content("Items in cart: 3")
+
+        within("#product-1") do
+          click_button "Remove Product"
+        end
+      
+        within("#product-2") do
+          click_button "Remove Product"
+        end
+
+        expect(page).to have_content("Items in cart: 1")
+    end
 end
