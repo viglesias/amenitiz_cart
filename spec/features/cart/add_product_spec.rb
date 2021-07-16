d485012d96d513dbf41f90711906687d0f41f413
Victorio Iglesias Gutierrez <victorio.iglesias@the-cocktail.com>
2021-07-16 08:02:32 +0200

    Test for items counter in cart

--- spec/features/cart/add_product_spec.rb
+++ spec/features/cart/add_product_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe "When a user adds a product to the cart" do
 
     expect(page).to have_content("Product added successfully")
   end
+
+  it "Increments cart items counter" do
+    product = Product.create(code: 'GR1', name: "Green Tea", price: 3.11)
+    product2 = Product.create(code: 'SR1', name: "Strawberries", price: 5.00)
+    product3 = Product.create(code: 'CF1', name: "Coffe", price: 11.23)
+
+    visit "/products"
+
+    expect(page).to have_content("Items in cart: 0")
+
+    within("#product-1") do
+      click_button "Add Product"
+    end
+
+    within("#product-2") do
+      click_button "Add Product"
+    end
+
+    within("#product-1") do
+      click_button "Add Product"
+    end
+    
+    expect(page).to have_content("Items in cart: 3")
+
+  end
 end
