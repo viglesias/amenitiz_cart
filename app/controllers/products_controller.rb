44230b951e3e200024d55ae89052232cfcfa7819
Victorio Iglesias Gutierrez <victorio.iglesias@the-cocktail.com>
2021-07-16 08:09:44 +0200

    Add counter in session

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,10 @@
 class ProductsController < ApplicationController
-  
+
   # GET /products or /products.json
   def index
     @products = Product.all
+    session[:cart] ||= []
+    @cart_items_counter = session[:cart].count || 0
   end
 
 end
